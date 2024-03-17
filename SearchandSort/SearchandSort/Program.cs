1345a41284431ebb743d3b8109eadf84c286802d
Josh Williams <jlw1998@protonmail.com>
2024-03-17 16:49:16 +0000

    Added base code for reading files and asset files

---
+++ SearchandSort/SearchandSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchandSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName = "Net_1_2048.txt";
+            string directory = Path.Combine(Environment.CurrentDirectory, "..", "..", "Data"); // Move up two levels to get to the Data folder
+            string desiredPath = Path.Combine(directory, fileName);
+
+
+            Console.WriteLine("Actual path {0}", desiredPath);
+            Console.WriteLine("Desired path C:\\Users\\jlw19\\Desktop\\Algorithms-Complexity\\SearchandSort\\SearchandSort\\Data\\Net_1_2048.txt");
+
+
+            string contents = File.ReadAllText(desiredPath);
+
+            Console.WriteLine("{0}", contents);
+
+            Console.ReadLine();
+     
+        }
+    }
+}
